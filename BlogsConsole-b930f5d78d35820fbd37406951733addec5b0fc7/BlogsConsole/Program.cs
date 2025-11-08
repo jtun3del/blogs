b3c11a174d175382e987566d66755eba09735531
lamine <jdjdj>
2025-11-08 14:43:46 -0600

    menu created

--- BlogsConsole-b930f5d78d35820fbd37406951733addec5b0fc7/BlogsConsole/Program.cs
+++ BlogsConsole-b930f5d78d35820fbd37406951733addec5b0fc7/BlogsConsole/Program.cs
@@ -10,94 +10,116 @@ logger.Info("Program started");
 
 //create the data context
 var db = new DataContext();
+var x = 1;
+while (x != 5)
+{
+  Console.WriteLine("Press 1 to display all blogs \n" +
+                    "press 2 to add a new blog \n " +
+                    "press 3 to add a new post\n" +
+                    "press 4 to display all posts\n" +
+                    "press 5 to quit\n");
+
+  x = int.Parse(Console.ReadLine());
+
+  if (x == 2)
+  {
 // Create and save a new Blog
 
 
-Console.Write("Enter a name for a new Blog: ");
+    Console.Write("Enter a name for a new Blog: ");
 
 
-var name = Console.ReadLine();
-
-
-
-
-
-var blog = new Blog { Name = name };
+    var name = Console.ReadLine();
 
 
 
 
 
-
-
-
-db.AddBlog(blog);
-
-
-logger.Info("Blog added - {name}", name);
+    var blog = new Blog { Name = name };
 
 
 
 
 
+
+
+
+    db.AddBlog(blog);
+
+
+    logger.Info("Blog added - {name}", name);
+
+
+  }
+
+  if (x == 1)
+  {
 // Display all Blogs from the database
 
 
-var query = db.Blogs.OrderBy(b => b.Name);
+    var query = db.Blogs.OrderBy(b => b.Name);
 
 
 
 
 
-Console.WriteLine("All blogs in the database:");
+    Console.WriteLine("All blogs in the database:");
 
 
-foreach (var item in query)
+    foreach (var item in query)
 
 
-{
+    {
 
 
-  Console.WriteLine(item.Name);
+      Console.WriteLine(item.Name);
 
 
-}
+    }
+  }
+
+  if (x == 3)
+  {
 //create post
-Console.WriteLine("what Blog are you posting to");
+    Console.WriteLine("what Blog are you posting to");
 
-var targetBlog = db.Blogs.Where(b => b.Name == Console.ReadLine());
-Console.WriteLine("Enter a title for a new Post: ");
+    var targetBlog = db.Blogs.Where(b => b.Name == Console.ReadLine());
+    Console.WriteLine("Enter a title for a new Post: ");
 
-var postTitle = Console.ReadLine();
+    var postTitle = Console.ReadLine();
 
-Console.WriteLine("Enter content for a new Post:");
-var postContent = Console.ReadLine();
+    Console.WriteLine("Enter content for a new Post:");
+    var postContent = Console.ReadLine();
 
-var post = new Post { Title = postTitle, Content = postContent };
-db.AddPost(targetBlog.First(), post);
+    var post = new Post { Title = postTitle, Content = postContent };
+    db.AddPost(targetBlog.First(), post);
+  }
 
+  if (x == 4)
+  {
 //display all posts
 
-Console.WriteLine("what blogs posts do you want to see");
-var targBlog = db.Blogs.Where(b => b.Name == Console.ReadLine());
+    Console.WriteLine("what blogs posts do you want to see");
+    var targBlog = db.Blogs.Where(b => b.Name == Console.ReadLine());
 
-var postsquery = db.Posts.Where(b => b.Blog == targBlog.First());
+    var postsquery = db.Posts.Where(b => b.Blog == targBlog.First());
 
-Console.WriteLine($"All posts in the database: {postsquery.Count()}");
-
-
-foreach (var item in postsquery)
+    Console.WriteLine($"All posts in the database: {postsquery.Count()}");
 
 
-{
+    foreach (var item in postsquery)
 
 
-  Console.WriteLine(item.Blog.Name, item.Title, item.Content);
+    {
 
 
+      Console.WriteLine(item.Blog.Name, item.Title, item.Content);
+
+
+    }
+
+
+  }
 }
-
-
-
 
 logger.Info("Program ended");
